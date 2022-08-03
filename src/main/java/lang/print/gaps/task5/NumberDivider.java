f64517210d38d283e5530ab02f127841ec3d7373
IGAZIEV <islam.gaziev@gmail.com>
2022-08-03 22:03:30 +0500

    Module #3. Task #5. Attempt #2.

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -2,7 +2,7 @@ package lang.print.gaps.task5;
 
 public class NumberDivider {
     void divide(int toBeDivided) {
-        float result = toBeDivided / 5;
+        float result = toBeDivided / 5.0f;
         System.out.println(result);
     }
 }
